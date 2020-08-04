11d93a6ea2e624b43e6477b4cea51d31f28e3208
Jimmy Lin <jimmylin@Lillians-MacBook-Pro-2018.local>
2020-08-03 17:49:13 -0700

    Setup App with Card, Chart and CountryPicker components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { fetchData } from './components/api';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetchData().then((data) => {
+      setData({ data });
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Cards />
+      <CountryPicker />
+      <Chart />
     </div>
   );
 }
